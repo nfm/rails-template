8208713c0360acdd51d6bbc6036ac1f9f620ab94
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2015-04-23 09:50:31 +1000

    Set up Sprockets ES6

--- template.rb
+++ template.rb
@@ -16,3 +16,15 @@ copy_file 'config/puma.rb'
 
 # Set up Procfile
 copy_file 'Procfile'
+
+# Set up sidekiq
+environment "config.active_job.queue_adapter = :sidekiq"
+
+# Set up Sprockets ES6
+copy_file 'app/assets/javascripts/application.js'
+environment <<-CODE
+# Use system-js for Sprockets ES6 modules
+Rails.application.config.assets.configure do |env|
+  env.register_transformer 'text/ecmascript-6', 'application/javascript', Sprockets::ES6.new('modules' => 'system', 'moduleIds' => true)
+end
+CODE
